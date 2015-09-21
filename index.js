44941f30b7c40f6fd978ec48c94879a7e076b297
ErikR <erik.ronstrom@doremir.com>
2015-09-21 09:25:29 +0200

    Updated to the new coord semantics [steps, semitones]

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-// First coord is octaves, second is fifths. Distances are relative to c
+// First coord is diatonic steps, second is semitones. Distances are relative to c
 var notes = {
   c: [0, 0],
-  d: [-1, 2],
-  e: [-2, 4],
-  f: [1, -1],
-  g: [0, 1],
-  a: [-1, 3],
-  b: [-2, 5],
-  h: [-2, 5]
+  d: [1, 2],
+  e: [2, 4],
+  f: [3, 5],
+  g: [4, 7],
+  a: [5, 9],
+  b: [6, 11],
+  h: [6, 11]
 };
 
 module.exports = function(name) {
@@ -15,5 +15,5 @@ module.exports = function(name) {
 };
 
 module.exports.notes = notes;
-module.exports.A4 = [3, 3]; // Relative to C0 (scientic notation, ~16.35Hz)
-module.exports.sharp = [-4, 7];
+module.exports.A4 = [33, 57]; // Relative to C0 (scientic notation, ~16.35Hz)
+module.exports.sharp = [0, 1];
